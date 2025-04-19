861145f7bfc87dafe6bda2acbd95c6678a41c3b1
babyoiii <danhlam2710@gmail.com>
2025-04-19 21:11:41 +0700

    Component TroGiup

---
+++ fe-datn-customer/src/app/Component/refund/yeu-cau-hoan-tien/yeu-cau-hoan-tien.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yeu-cau-hoan-tien',
+  standalone: true,
+  imports: [],
+  templateUrl: './yeu-cau-hoan-tien.component.html',
+  styleUrl: './yeu-cau-hoan-tien.component.css'
+})
+export class YeuCauHoanTienComponent {
+
+}
---
+++ fe-datn-customer/src/app/Component/refund/yeu-cau-xac-nhan/yeu-cau-xac-nhan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yeu-cau-xac-nhan',
+  standalone: true,
+  imports: [],
+  templateUrl: './yeu-cau-xac-nhan.component.html',
+  styleUrl: './yeu-cau-xac-nhan.component.css'
+})
+export class YeuCauXacNhanComponent {
+
+}
